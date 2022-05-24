4195233c44041a351061ffe526a8778a044eeb23
NZ-WEB <stereo00@yandex.ru>
2022-05-24 22:31:19 +0300

    fixed ui in appOwnMemberPageInfoItem

--- quasar-project/src/components/AppOwnMemberPageInfoItem.vue
+++ quasar-project/src/components/AppOwnMemberPageInfoItem.vue
@@ -1,6 +1,9 @@
 <template>
   <q-item-label class="q-my-sm">
-    <span v-if="!isEditing">{{ literName }}: {{ item }}</span>
+    <div v-if="!isEditing">
+      <span class="text-body1">{{ literName }}: {{ item }}</span>
+      <q-separator class="q-my-sm"/>
+    </div>
     <q-input
       outlined
       v-else
